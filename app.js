829f363e3c679bddd4088f60e473e0f9fb715950
Tung Mai <tungm92@gmail.com>
2024-08-06 16:31:00 -0400

    finished exercises

--- app.js
+++ app.js
@@ -105,8 +105,75 @@ console.log('Exercise 6: ', userProfile);
 
 const sentence = (animal = 'cat', color = 'white') => {
     console.log(`The ${animal} is ${color}`)
-}
+};
 
-console.log('Exercise 8: ')
-sentence()
-sentence('dog', 'black')
+console.log('Exercise 8: ');
+sentence();
+sentence('dog', 'black');
+
+// Exercise 9
+
+// Convert the following `if...else` statement in to a ternary:
+
+let pizza = 'tasty';
+
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
+
+// Your code here
+
+const taste = pizza === 'tasty' ? 'yum' : 'yuck';
+console.log("Exercise 9: ", taste)
+
+// Exercise 10
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+const LANG = localLangConfig || 'en';
+
+// Log the result
+console.log('Exercise 10: ')
+console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+
+const USER_THEME = userSavedTheme || 'light';
+
+// Log the result
+console.log('User theme setting:', USER_THEME);
+
+
+// Exercise 11
+
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+    name: 'Alice',
+  };
+  
+let cat; // Your code here
+
+cat = adventurer.cat?.name
+
+console.log('Exercise 11: ', cat);
